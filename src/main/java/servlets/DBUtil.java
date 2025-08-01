2f3fa5c80790df0acd9edc7db7e49e3aa408171d
Anant mishra <anant3380@gmail.com>
2025-08-01 18:32:11 +0530

    Update DBUtil.java

--- src/main/java/servlets/DBUtil.java
+++ src/main/java/servlets/DBUtil.java
@@ -6,6 +6,6 @@ import java.sql.*;
 public class DBUtil {
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/student", "root", "");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/student", "root", "Anant@2004");
     }
 }
